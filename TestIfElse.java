aebaee84e84ccec9f60e97c1d98323e8d225cd98
narisu <narisunarisu20@gamil.com>
2023-04-05 15:01:42 +0900

    那日苏

---
+++ TestIfElse.java
@@ -0,0 +1,15 @@
+public class TestIfElse {
+	public static void main(String[] args) {
+		//int x = 10;
+		int x = 30;
+		if(x < 20) {
+			System.out.println("这是if语句");
+		}else {
+			System.out.println("这是else语句");
+		}
+	//	if(x < 20) {
+		//	System.out.println("这是if语句");
+	//	}
+	}
+}
+
