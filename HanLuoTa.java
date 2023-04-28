7b9e05618ad88b7790d8309c522df7288ed0ec13
narisu <narisunarisu20@gamil.com>
2023-04-28 15:46:21 +0900

    narisu

---
+++ HanLuoTa.java
@@ -0,0 +1,18 @@
+public class HanLuoTa {
+	public static void main(String[] args) {
+		Tower tower = new Tower();
+		tower.move(2, 'A', 'B', 'C');
+	}
+}
+
+class Tower {
+	public void move(int num, char a, char b, char c) {
+		if(num == 1) {
+			System.out.println(a + "=> "+ c);
+		} else {
+			move(num - 1,a,c,b);
+			System.out.println(a + "=>" + c);
+			move(num - 1,b,a,c);
+		}
+	}
+}
